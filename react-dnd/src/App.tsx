c3d9eafb27a758cc79462ba7928ebe3dc8bd78b7
sangyun lee <sororiri@gmail.com>
2024-09-24 17:59:53 +0900

    feat: sortable Component 예제 추가

--- react-dnd/src/App.tsx
+++ react-dnd/src/App.tsx
@@ -1,29 +1,56 @@
 import React, {useState} from 'react';
-import {DndContext} from '@dnd-kit/core';
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from '@dnd-kit/core';
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
 
-import {Droppable} from './Droppable';
-import {Draggable} from './Draggable';
+import {SortableItem} from './SortableItem';
 
-function App() {
-  const [isDropped, setIsDropped] = useState(false);
-  const draggableMarkup = (
-    <Draggable>Drag me</Draggable>
+export default function App() {
+  const [items, setItems] = useState([1, 2, 3]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
   );
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      {!isDropped ? draggableMarkup : null}
-      <Droppable>
-        {isDropped ? draggableMarkup : 'Drop here'}
-      </Droppable>
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext
+        items={items}
+        strategy={verticalListSortingStrategy}
+      >
+        {items.map(id => <SortableItem key={id} id={id} />)}
+      </SortableContext>
     </DndContext>
   );
 
-  function handleDragEnd(event: any) {
-    if (event.over && event.over.id === 'droppable') {
-      setIsDropped(true);
+  function handleDragEnd(event: DragEndEvent) {
+    const {active, over} = event;
+
+    if (active.id !== over!.id) {
+      setItems((items) => {
+        const oldIndex = items.findIndex((item) => item === active.id);
+        const newIndex = items.findIndex((item) => item === over!.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
     }
   }
 }
-
-export default App;
